227cc5379879da8c4bbe9651d098aca29a561aa5
pmagent2013 <pmagent2013@gmail.com>
2016-05-03 20:17:57 -0400

    fixed copy and paste

--- FormalLanguageMilestone2/src/FormalLanguageMilestone2Frame.java
+++ FormalLanguageMilestone2/src/FormalLanguageMilestone2Frame.java
@@ -23,6 +23,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class FormalLanguageMilestone2Frame extends JFrame {
 
@@ -67,6 +70,33 @@ public class FormalLanguageMilestone2Frame extends JFrame {
         getContentPane().add(contentPane);
 
         pack();
+        
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        
+        JMenu mnFile = new JMenu("File");
+        menuBar.add(mnFile);
+        
+        JMenuItem mntmSave = new JMenuItem("Save");
+        mnFile.add(mntmSave);
+        
+        JMenuItem mntmLoad = new JMenuItem("Load");
+        mnFile.add(mntmLoad);
+        
+        JMenu mnEdit = new JMenu("Edit");
+        menuBar.add(mnEdit);
+        
+        JMenuItem mntmUndo = new JMenuItem("Undo");
+        mnEdit.add(mntmUndo);
+        
+        JMenuItem mntmRedo = new JMenuItem("Redo");
+        mnEdit.add(mntmRedo);
+        
+        JMenu mnPreferences = new JMenu("Preferences");
+        menuBar.add(mnPreferences);
+        
+        JMenuItem mntmColorScheme = new JMenuItem("Color Scheme");
+        mnPreferences.add(mntmColorScheme);
         setVisible(true);   
 		
 		
@@ -89,27 +119,27 @@ public class FormalLanguageMilestone2Frame extends JFrame {
 		 for(int i = 0; i < statesVisited.size(); i++){
 			 Document doc = textPane.getDocument();
 			 try {
-				charToAdd = doc.getText(doc.getLength() - 1, 1);	
-				doc.remove(doc.getLength()-1, 1);
+				charToAdd = doc.getText(i, 1);	
+				doc.remove(i, 1);
 			} catch (BadLocationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			 if(statesVisited.get(i) == 1 || statesVisited.get(i) == 2 || statesVisited.get(i) == 3 || statesVisited.get(i) == 4 || statesVisited.get(i) == 5){
-				 appendToPane(textPane, charToAdd, Color.BLUE, doc.getLength());
+				 appendToPane(textPane, charToAdd, Color.BLUE, i);
 			 }
 			 else if(statesVisited.get(i) == 32 || statesVisited.get(i) == 33 || statesVisited.get(i) == 34 || statesVisited.get(i) == 35
 					 || statesVisited.get(i) == 36 || statesVisited.get(i) == 37 || statesVisited.get(i) == 38){
-				 appendToPane(textPane, charToAdd, Color.GREEN, doc.getLength());
+				 appendToPane(textPane, charToAdd, Color.GREEN, i);
 			 }
 			 else if(statesVisited.get(i) == 24 || statesVisited.get(i) == 25 || statesVisited.get(i) == 26 || statesVisited.get(i) == 10
 					 || statesVisited.get(i) == 11 || statesVisited.get(i) == 12 || statesVisited.get(i) == 13 || statesVisited.get(i) == 9
 					 || statesVisited.get(i) == 14 || statesVisited.get(i) == 44 || statesVisited.get(i) == 45 || statesVisited.get(i) == 46
 					 || statesVisited.get(i) == 47 || statesVisited.get(i) == 48){
-				 appendToPane(textPane, charToAdd, Color.CYAN, doc.getLength());
+				 appendToPane(textPane, charToAdd, Color.CYAN, i);
 			 }
 			 else if(statesVisited.get(i) == 29 || statesVisited.get(i) == 28 || statesVisited.get(i) == 21 || statesVisited.get(i) == 39){
-				 appendToPane(textPane, charToAdd, Color.MAGENTA, doc.getLength());
+				 appendToPane(textPane, charToAdd, Color.MAGENTA, i);
 			 }
 			 else if(statesVisited.get(i) == 31 || statesVisited.get(i) == 7 || statesVisited.get(i) == 9 || statesVisited.get(i) == 13
 					 || statesVisited.get(i) == 48 || statesVisited.get(i) == 18 || statesVisited.get(i) == 23 || statesVisited.get(i) == 20
@@ -117,10 +147,10 @@ public class FormalLanguageMilestone2Frame extends JFrame {
 					 || statesVisited.get(i) == 19 || statesVisited.get(i) == 99 || statesVisited.get(i) == 6 || statesVisited.get(i) == 16 
 					 || statesVisited.get(i) == 40 || statesVisited.get(i) == 41 || statesVisited.get(i) == 43 || statesVisited.get(i) == 27
 					 || statesVisited.get(i) == 22 || statesVisited.get(i) == 17 || statesVisited.get(i) == 50 || statesVisited.get(i) == 51){
-				 appendToPane(textPane, charToAdd, Color.BLACK, doc.getLength());
+				 appendToPane(textPane, charToAdd, Color.BLACK, i);
 			 }
 			 else{
-				 appendToPane(textPane, charToAdd, Color.RED, doc.getLength());
+				 appendToPane(textPane, charToAdd, Color.RED, i);
 			 }
 			 
 		 }
